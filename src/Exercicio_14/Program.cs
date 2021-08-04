dbfbf530c1b14f6f25f48f4250ca3847161541cd
RaulORios <raul.orios89@outlook.com>
2021-08-04 18:32:32 -0300

    Program.cs adicionado

---
+++ src/Exercicio_14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercicio_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************");
+            Console.WriteLine("*********     Exercicio 14     *********");
+            Console.WriteLine("****************************************");
+
+            Console.WriteLine("");
+
+            int numero;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            if (numero < 10)
+            {
+                Console.WriteLine($"O número {numero} é MENOR que 10.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} é MAIOR que 10.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
